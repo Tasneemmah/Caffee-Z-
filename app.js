b1d48a9905c29560874fe55b1360d3ba1d3dd560
Tasneemmah <tasneemmahmuod2@gmail.com>
2023-03-25 00:30:27 -0700

    taske

--- app.js
+++ app.js
@@ -1,50 +1,98 @@
-       let person = prompt("Please enter your name");
-       let age=prompt("please enter your age");
-       let gender=prompt("please enter your gender","male or female")
+     // let customerName = prompt("Please enter your name");
 
-       function genderf(gender){
-          while  (gender != "male" && gender!="female"){
-          gender= prompt("enter your gender ")
 
-        let genderCapital = gender.toUpperCase();
+    // let customerGender = prompt("Please enter your Gender male/female")
+    // function specifyGender(customerGender) {
 
-         if (genderCapital == "MALE") {
-             alert("Welcome to MyCoffee Mr"+" "+person);
-            }
-        else if(genderCapital == "FEMALE"){
+    // while (customerGender != "male" && customerGender != "female") {
+    // customerGender = prompt("Please enter your Gender male/female")
+    //     }
 
-             alert("Welcome to MyCoffee MS"+" "+person);
-            }
-         } 
-      }
+    // if (customerGender == "male") {
+    //     alert("Welcom Mr " + customerName)
+    //     } else {
+    //      alert("Welcom Ms " + customerName)
+    //     }
+    // }
+    // specifyGender(customerGender)
 
-           genderf(gender)
-           let drink=prompt('Do you like a hot or cold coffe? ')
-           let drinkName=prompt('nice,what is the drink you want? ')
+    // let hotColdDrink = prompt("do you want your drink hot or cold ?")
 
-            alert('Please wait,your drink is being prepared')
+    // let typeDrink = prompt("please write the name of the drink you want")
 
-             let order = document.getElementById("order");
-             let paarq = document.createElement("p");
-             let mainul = document.createElement("ul");
-             let li1 = document.createElement("li");
-             let li2 = document.createElement("li");
-             let li3 = document.createElement("li");
+    // alert("please wait the drink is getting prepared ")
 
-            paarq.textContent=person;
-            li1.textContent=gender;
-            li2.textContent=age;
-            li3.textContent=drink , " ",drinkName;
+    // if (customerGender == "male") {
+    //     console.log("ok Mr " + customerName + " your order is " + hotColdDrink + " " + typeDrink)
+    //     } else if (customerGender == "female") {
+    //     console.log("ok Ms " + customerName + " your order is " + hotColdDrink + " " + typeDrink)
+    //     } else {
+    //     console.log("ok " + customerName + " your order is " + hotColdDrink + " " + typeDrink)
+    // }
 
-            mainul.appendChild(li1);
-            mainul.appendChild(li2);
-            mainul.appendChild(li3);
-            order.appendChild(paarq);
-            order.appendChild(mainul);
+    // let customer = [customerName, customerGender, hotColdDrink, typeDrink]
 
-           let customerArray=[person,age,gender,drink,drinkName]
+    // for (let i = 0; i < customer.length; i++) {
+    //     console.log(customer[i])
+    // }
 
-           for (let index = 0; index < customerArray.length; index++) {
+    // let userInputs = document.getElementById("user-inputs");
+    // let div = document.createElement("div");
+    // let paragraph = document.createElement("p");
+    // let ul = document.createElement("ul");
+    // let li1 = document.createElement("li");
+    // let li2 = document.createElement("li");
+    // let li3 = document.createElement("li");
 
-        console.log(customerArray[index])
-}
+
+    // userInputs.appendChild(div);
+    // div.appendChild(paragraph);
+    // div.appendChild(ul);
+    // ul.appendChild(li1);
+    // ul.appendChild(li2);
+    // ul.appendChild(li3);
+
+
+    // paragraph.textContent = `${customerName}`;
+    // li1.textContent = ` Gender : ${customerGender}`;
+    // li2.textContent = ` Age : 26`;
+    // li3.textContent = ` Drink : ${hotColdDrink} ${typeDrink}`;
+
+    let form = document.getElementById("formOrder")
+    let userInputs = document.getElementById("user-inputs");
+
+    form.addEventListener("submit", function (event) {
+       event.preventDefault()
+       let userName = event.target.userName.value
+       let age = event.target.age.value
+       let cold = event.target.cold.checked
+       let hot = event.target.hot.checked
+       let drinkName = event.target.drinkName.value
+       let drink = ""
+        if (cold) {
+          drink = drink + "cold "
+       }
+        if (hot) {
+         drink = drink + " hot"
+       }
+        console.log(userName, age, cold, hot, drinkName)
+        render(userName,age,drink,drinkName)
+       })
+
+       function render(userName, age, drink , drinkName) {
+
+       let h4 = document.createElement("h4")
+       let pName = document.createElement("p")
+       let pAge = document.createElement("p")
+       let pDrink = document.createElement("p")
+
+       h4.textContent = `"Your Order "`
+       pName.textContent = `${userName}`
+       pAge.textContent = `${age}`
+       pDrink.textContent = `${drink} ${drinkName} `
+
+       userInputs.appendChild(h4)
+       userInputs.appendChild(pName)
+       userInputs.appendChild(pAge)
+       userInputs.appendChild(pDrink)
+   }
